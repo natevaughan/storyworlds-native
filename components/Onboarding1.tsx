f3fd2e2c8f6841f98e49be3792132cfe4ff1c463
Nate Vaughan <nate.vaughan@protonmail.com>
2021-06-14 18:04:30 -0500

    onboarding styles

--- components/Onboarding1.tsx
+++ components/Onboarding1.tsx
@@ -11,8 +11,18 @@ interface OnboardingProps {
 const Onboarding1: FC<OnboardingProps> = () => {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>welcome to storyworlds</Text>
-            <Text style={styles.subtitle}>collaborative interactive fiction</Text>
+            <View>
+                <Text style={styles.title}>welcome to storyworlds</Text>
+                <Text style={styles.subtitle}>collaborative interactive fiction</Text>
+            </View>
+            <View style={styles.info}>
+                <Text>play interactive, text-based stories</Text>
+            </View>
+            <View style={styles.playContainer}>
+                <Text style={styles.worldTitle}>Mars</Text>
+                <Text style={styles.play}>&gt; fly north</Text>
+                <Text style={styles.play}>You enter an asteroid belt</Text>
+            </View>
         </View>
     );
 }
@@ -31,5 +41,35 @@ const styles = StyleSheet.create({
     },
     subtitle: {
         fontSize: 18,
-    }
+    },
+    info: {
+        marginRight: "45%",
+        marginLeft: "5%",
+        marginTop: 30,
+        marginBottom: -20,
+        textAlign: "left",
+        width: "50%",
+        borderColor: "#dddddd",
+        borderWidth: 1,
+        padding: 10,
+        zIndex: 100,
+        backgroundColor: "#ffffff"
+    },
+    playContainer: {
+        marginRight: "5%",
+        marginLeft: "45%",
+        textAlign: "left",
+        width: "50%",
+        padding: 10,
+        backgroundColor: "#111166",
+    },
+    play: {
+        color: "#ffffff"
+    },
+    worldTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: "#ffffff"
+    },
 });
--- components/Onboarding2.tsx
+++ components/Onboarding2.tsx
@@ -4,8 +4,27 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Onboarding2() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Edit and expand stories created by others as you play them</Text>
-      <View style={styles.separator} />
+      <View style={styles.info}>
+        <Text>Edit and expand stories created by others as you play them</Text>
+      </View>
+      <View style={styles.playContainer}>
+        <Text style={styles.worldTitle}>Mars</Text>
+        <Text style={styles.play}>&gt; edit location</Text>
+      </View>
+      <View style={[styles.editContainer]}>
+        <Text style={styles.subtitle}>Edit location</Text>
+        <View style={styles.row}>
+          <Text>Title</Text>
+          <Text style={styles.editableBox}>Asteroid belt</Text>
+        </View>
+        <View style={styles.row}>
+          <Text>Description</Text>
+          <Text style={styles.editableBox}>You enter an asteroid belt</Text>
+        </View>
+        <View style={styles.saveButton}>
+          <Text style={styles.saveButtonText}>Save</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -20,9 +39,79 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  subtitle: {
+    fontSize: 18,
   },
+  info: {
+    marginRight: "45%",
+    marginLeft: "5%",
+    marginTop: 30,
+    marginBottom: -20,
+    textAlign: "left",
+    width: "50%",
+    borderColor: "#dddddd",
+    borderWidth: 1,
+    padding: 10,
+    zIndex: 100,
+    backgroundColor: "#ffffff",
+  },
+  playContainer: {
+    marginRight: "5%",
+    marginLeft: "45%",
+    textAlign: "left",
+    width: "50%",
+    padding: 10,
+    backgroundColor: "#111166",
+    minHeight: 200,
+  },
+  play: {
+    color: "#ffffff"
+  },
+  worldTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: "#ffffff"
+  },
+  editContainer: {
+    borderRadius: 10,
+    marginRight: "10%",
+    marginLeft: "10%",
+    textAlign: "left",
+    width: "80%",
+    borderColor: "#dddddd",
+    borderWidth: 1,
+    padding: 10,
+    zIndex: 100,
+    backgroundColor: "#ffffff",
+    marginTop: -100
+  },
+  saveButton: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 5,
+    borderRadius: 5,
+    backgroundColor: '#339933',
+    marginTop: 5,
+    width: "50%",
+    marginLeft: "25%",
+    marginRight: "25%",
+  },
+  saveButtonText: {
+    color: "#ffffff",
+    textAlign: "center",
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 5,
+  },
+  editableBox: {
+    padding: 4,
+    borderWidth: 1,
+    borderColor: "#333333",
+    minWidth: 150,
+  }
 });
