84cc78208d250b9782e73ba4a7223740ec6812e8
Nate Vaughan <nate.vaughan@protonmail.com>
2021-06-14 13:55:32 -0500

    fix dead link

--- components/Onboarding1.tsx
+++ components/Onboarding1.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, FC } from 'react';
 import { StyleSheet } from 'react-native';
 import { View, Text, PanResponderGestureState } from 'react-native';
 import GestureRecognizer from 'react-native-swipe-gestures';
@@ -8,75 +8,16 @@ interface OnboardingProps {
     navigation: any
 }
 
-export default class Onboarding1 extends Component<OnboardingProps> {
-
-    constructor(props: OnboardingProps) {
-        super(props);
-    }
-
-    onSwipeUp(gestureState: PanResponderGestureState) {
-        console.log('SwipeUp')
-    }
-
-    onSwipeDown(gestureState: PanResponderGestureState) {
-        console.log('SwipeDown')
-    }
-
-    onSwipeLeft(gestureState: PanResponderGestureState) {
-        this.props.navigation.push('TabTwo')
-    }
-
-    onSwipeRight(gestureState: PanResponderGestureState) {
-        console.log('SwipeRight')
-    }
-
-    onSwipe(gestureName: string, gestureState: PanResponderGestureState) {
-        // @ts-ignore
-        const {SWIPE_UP, SWIPE_DOWN, SWIPE_LEFT, SWIPE_RIGHT} = swipeDirections;
-        this.setState({gestureName: gestureName});
-        switch (gestureName) {
-            case SWIPE_UP:
-                break;
-            case SWIPE_DOWN:
-                break;
-            case SWIPE_LEFT:
-                break;
-            case SWIPE_RIGHT:
-                break;
-        }
-    }
-
-    render() {
-
-        const config = {
-            velocityThreshold: 0.3,
-            directionalOffsetThreshold: 80
-        };
-
-        return (
-            <GestureRecognizer
-                onSwipe={(direction, state) => this.onSwipe(direction, state)}
-                onSwipeUp={(state) => this.onSwipeUp(state)}
-                onSwipeDown={(state) => this.onSwipeDown(state)}
-                onSwipeLeft={(state) => this.onSwipeLeft(state)}
-                onSwipeRight={(state) => this.onSwipeRight(state)}
-                config={config}
-                style={{
-                    flex: 1
-                }}
-            >
-                <View style={styles.container}>
-                    <Text style={styles.title}>welcome to storyworlds</Text>
-                    <Text style={styles.subtitle}>collaborative interactive fiction</Text>
-                    <Text style={styles.link} onPress={() => {
-                        console.log("tapped next")
-                        this.props.navigation.push('TabTwo')
-                    }}>next</Text>
-                </View>
-            </GestureRecognizer>
-        );
-    }
+const Onboarding1: FC<OnboardingProps> = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>welcome to storyworlds</Text>
+            <Text style={styles.subtitle}>collaborative interactive fiction</Text>
+        </View>
+    );
 }
+
+export default Onboarding1;
 
 const styles = StyleSheet.create({
     container: {
@@ -90,14 +31,5 @@ const styles = StyleSheet.create({
     },
     subtitle: {
         fontSize: 18,
-    },
-    link: {
-        fontSize: 18,
-        textDecorationLine: 'underline'
-    },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: '80%',
-    },
+    }
 });
